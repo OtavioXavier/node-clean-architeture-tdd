e91d4063da7cef215390084943f2e24785b67223
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-01-22 20:54:52 -0300

    :bricks: chore: add jest configurations

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 const config = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8'
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['**/*src/**/*.js']
 }
 
 export default config
