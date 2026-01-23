76cf6519c0e1baf371f17a0b79508b9e9db93617
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-01-22 22:04:10 -0300

    :test_tube: text: ensure AuthUseCase throws missing-param-error if no email is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,7 +1,9 @@
+const { MissingParamError } = require('../../utils/errors')
+
 class AuthUseCase {
   async auth (email) {
     if (!email) {
-      throw new Error()
+      throw new MissingParamError('email')
     }
   }
 }
@@ -10,6 +12,6 @@ describe('AuthUseCase', () => {
   test('Should throw if no email is provided', async () => {
     const sut = new AuthUseCase()
     const accessToken = sut.auth()
-    await expect(accessToken).rejects.toThrow()
+    await expect(accessToken).rejects.toThrow(new MissingParamError('email'))
   })
 })
