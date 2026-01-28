f2f0f58c65a90136670cdea12fe1ac2204a0ffe6
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-01-28 18:08:52 -0300

    :test_tube: test[token-generator]: ensure TokenGenerator call jwt with correct values

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -31,6 +31,7 @@ describe('Token Generator', () => {
   test('should call JWT with correct values', async () => {
     const sut = makeSut()
     await sut.generate('any_id')
+    expect(jwt.id).toBe('any_id')
     expect(jwt.secret).toBe(sut.secret)
   })
 })
