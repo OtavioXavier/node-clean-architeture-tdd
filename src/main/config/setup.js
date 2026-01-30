83ca4141ecad5cd98999f3f8e6e3a4aaca59fe82
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-01-30 08:09:15 -0300

    :test_tube: test[main] ensure cors is enable

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,7 @@
 module.exports = app => {
   app.disable('x-powered-by')
+  app.use((req, res, next) => {
+    res.set('access-control-allow-origin', '*')
+    next()
+  })
 }
--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -3,9 +3,16 @@ import app from './app'
 
 describe('App setup', () => {
   test('should disable x-powered-by header', async () => {
-    app.get('/test-x-powered-by', (req, res) => res.send(''))
+    app.get('/test_x_powered-by', (req, res) => res.send(''))
 
     const res = await request(app).get('/test-x-powered-by')
     expect(res.headers['x-powered-by']).toBeUndefined()
+  })
+
+  test('should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => res.send(''))
+
+    const res = await request(app).get('/test_cors')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
   })
 })
