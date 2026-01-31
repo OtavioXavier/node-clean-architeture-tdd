98ad4c6df2662ad03ad43229cf34b5cfd9cc8b95
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-01-31 10:48:37 -0300

    :sparkles: feat[routes]: add routes file

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const appSetup = require('./setup')
+const setupRoutes = require('./routes')
 
 const app = express()
 appSetup(app)
+setupRoutes(app)
 
 module.exports = app
--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoUrl: process.env.MOGO_URL || 'mongodb://lacalhost:27017/clean-node-api'
+  mongoUrl: process.env.MOGO_URL || 'mongodb://localhost:27017/clean-node-api'
 }
