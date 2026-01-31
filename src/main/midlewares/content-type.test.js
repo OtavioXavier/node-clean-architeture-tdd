8e0ddb128a73ebdadf09091816bede1fc19e57dd
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-01-30 23:30:09 -0300

    :test_tube: test[content-type] ensure request return content-type as json as default

---
+++ src/main/midlewares/content-type.test.js
@@ -0,0 +1,11 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Content-Type middleware', () => {
+  test('Should return json content-type as default', async () => {
+    app.get('/test_content_type', (req, res) => res.send({}))
+
+    await request(app).get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+})
