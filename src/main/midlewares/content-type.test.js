053ffea3e080b5ee5ff916f27fef2b61b344199e
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-01-30 23:34:41 -0300

    :test_tube: test[content-type] ensure request return content-type as xml if force

--- src/main/midlewares/content-type.test.js
+++ src/main/midlewares/content-type.test.js
@@ -3,9 +3,19 @@ import app from '../config/app'
 
 describe('Content-Type middleware', () => {
   test('Should return json content-type as default', async () => {
-    app.get('/test_content_type', (req, res) => res.send({}))
+    app.get('/test_content_type', (req, res) => res.send(''))
 
     await request(app).get('/test_content_type')
       .expect('content-type', /json/)
+  })
+
+  test('Should return xml content-type if force', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app).get('/test_content_type_xml')
+      .expect('content-type', /xml/)
   })
 })
